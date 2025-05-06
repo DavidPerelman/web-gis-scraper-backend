2ae66859935ed203e67fb02ce0b90d8fc9fa9c56
DavidPerelman <53565536+DavidPerelman@users.noreply.github.com>
2025-05-06 21:33:58 +0300

    add vercel cors origin

--- app/main.py
+++ app/main.py
@@ -7,21 +7,24 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="GIS Scraper API",
     description="API לקבלת פוליגון, ביצוע גרידת נתונים והחזרת שכבת תוצאה",
-    version="0.1.0"
+    version="0.1.0",
 )
-
-origins = os.getenv("CORS_ORIGINS", "").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:3000",
+        "https://web-gis-scraper.vercel.app",
+    ],  # או ["*"] רק זמנית בזמן פיתוח
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
 @router.get("/")
 async def main():
-    return {'message': 'Wellcome to web-gis-scraper api!'}
+    return {"message": "Wellcome to web-gis-scraper api!"}
+
 
 app.include_router(router)
