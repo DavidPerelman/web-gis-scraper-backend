34fe6a0aa3de0488e1e04002f0bb8bc8da672606
DavidPerelman <53565536+DavidPerelman@users.noreply.github.com>
2025-05-06 20:40:23 +0300

    preparation to upload to railway

--- server/app/main.py
+++ server/app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from app.api.routes import router
 
@@ -9,9 +10,11 @@ app = FastAPI(
     version="0.1.0"
 )
 
+origins = os.getenv("CORS_ORIGINS", "").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # או ["*"] לפיתוח
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
